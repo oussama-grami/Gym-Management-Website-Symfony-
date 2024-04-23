c219b7073d75ef931f3879b20b20aab81eddd487
Dhouibi Mohamed Aziz <mohamedaziz.dhouibi@insat.ucar.tn>
2024-04-23 20:43:50 +0100

    .

--- gymworld/src/Controller/TestController.php
+++ gymworld/src/Controller/TestController.php
@@ -8,9 +8,39 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class TestController extends AbstractController
 {
-    #[Route('/', name: 'app_test')]
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('test/index.html.twig');
+       /* return $this->render('test/index.html.twig');*/
+    }
+    #[Route('/accesscard',name:'app_accesscard')]
+    public function accesscard(): Response
+    {
+        /*return $this->render('test/accesscard.html.twig');*/
+    }
+    #[Route('/logout',name:'app_logout')]
+    public function logout(): Response
+    {
+        /*return $this->render('test/logout.html.twig');*/
+    }
+    #[Route('/services',name:'app_services')]
+    public function services(): Response
+    {
+        /*return $this->render('test/services.html.twig');*/
+    }
+    #[Route('/team',name:'app_team')]
+    public function team(): Response
+    {
+        /*return $this->render('test/team.html.twig');*/
+    }
+    #[Route('/contact',name:'app_contact')]
+    public function contact(): Response
+    {
+        /*return $this->render('test/contact.html.twig');*/
+    }
+    #[Route('/timetable',name:'app_timetable')]
+    public function timetable(): Response
+    {
+        /*return $this->render('test/timetable.html.twig');*/
     }
 }
